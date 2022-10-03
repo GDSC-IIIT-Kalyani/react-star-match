f2af2796646eae51a34a31391fac05b38fbbce89
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-10-03 22:06:03 +0530

    feat: add playagain and game component

--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -1,22 +1,20 @@
 import React from "react";
-import utils from "../MathUtils";
 
-const PlayNumber = props => (
-	<button className="number"
-    style = {{ backgroundColor: colors[props.status]}}
-    onClick={() => props.onClick('Num', props.number)}
-    
-    >
+const PlayNumber = (props) => (
+  <button
+    className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => props.onClick("Num", props.number)}
+  >
     {props.number}
   </button>
 );
 
-// Color Theme
 const colors = {
-    available: 'lightgray',
-    used: 'lightgreen',
-    wrong: 'lightcoral',
-    candidate: 'deepskyblue',
-  };
+  available: "lightgray",
+  used: "lightgreen",
+  wrong: "lightcoral",
+  candidate: "deepskyblue",
+};
 
 export default PlayNumber;
