96333aab519d2596de9b33355fec1962678505a9
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-10-03 22:18:33 +0530

    feat: add gameid to restart game after game over

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import StarsDisplay from './components/StarDisplay';
-import utils from './MathUtils';
-import PlayNumber from './components/PlayNumber';
-import PlayAgain from './components/PlayAgain';
+import React, { useState} from 'react';
+// import StarsDisplay from './components/StarDisplay';
+// import utils from './MathUtils';
+// import PlayNumber from './components/PlayNumber';
+// import PlayAgain from './components/PlayAgain';
 import Game from './components/Game';
 
+const StarMatch = () => {
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
+}
+
+export default StarMatch;
--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const PlayNumber = (props) => (
-  <button
-    className="number"
-    style={{ backgroundColor: colors[props.status] }}
-    onClick={() => props.onClick("Num", props.number)}
-  >
-    {props.number}
-  </button>
+const PlayNumber = props => (
+    <button
+      className="number"
+      style={{backgroundColor: colors[props.status]}}
+      onClick={() => props.onClick(props.number, props.status)}
+    >
+      {props.number}
+    </button>
 );
 
 const colors = {
